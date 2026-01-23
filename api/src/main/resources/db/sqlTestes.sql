91a9b5546a4c5a13cd5413881d4d2cefac31398b
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-01-22 22:15:40 -0300

    docs/Cria arquivo SQL de testes

---
+++ api/src/main/resources/db/sqlTestes.sql
@@ -0,0 +1,11 @@
+select * from medicos;
+
+-- visualiza migrations realizadas
+select * from flyway_schema_history;
+
+-- Excluir uma versão específica
+DELETE FROM flyway_schema_history WHERE version = 'numero da versão';
+
+-- Excluirá todas as migrations falhadas
+delete from flyway_schema_history where success = 0;
+
