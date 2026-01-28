bde8d87541fa7f8bd0dc2cca9845bb7baff49732
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-01-27 22:39:14 -0300

    Implementa criar paciente

---
+++ api/src/main/java/med/voll/api/Paciente/DadosCadastroPaciente.java
@@ -0,0 +1,27 @@
+package med.voll.api.Paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.Endereco.Endereco;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record DadosCadastroPaciente(
+
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String telefone,
+
+        @NotBlank
+//        @CPF // Desativado para testes pois bloqueou CPF inválido :D
+        String cpf,
+
+        @NotNull @Valid
+        Endereco endereco
+) {
+}
---
+++ api/src/main/java/med/voll/api/Paciente/Paciente.java
@@ -0,0 +1,39 @@
+package med.voll.api.Paciente;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.Endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String nome;
+    String email;
+    String telefone;
+    String cpf;
+
+    @Embedded
+    Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente paciente) {
+        this.nome = paciente.nome();
+        this.email = paciente.email();
+        this.telefone = paciente.telefone();
+        this.cpf = paciente.cpf();
+        this.endereco = paciente.endereco();
+    }
+}
