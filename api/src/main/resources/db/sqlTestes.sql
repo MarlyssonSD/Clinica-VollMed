0a165ef758c0c880a333716cf0a511e1654108a9
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-02-05 19:36:14 -0300

    feat/23_padroniza_retornos_requests_paciente

--- api/src/main/resources/db/sqlTestes.sql
+++ api/src/main/resources/db/sqlTestes.sql
@@ -10,3 +10,8 @@ DELETE FROM flyway_schema_history WHERE version = 'numero da versão';
 -- Excluirá todas as migrations falhadas
 delete from flyway_schema_history where success = 0;
 
+quebra
+-- Ativa todos os médicos
+UPDATE medicos med
+SET med.ativo = 1
+WHERE med.ativo = 0;
