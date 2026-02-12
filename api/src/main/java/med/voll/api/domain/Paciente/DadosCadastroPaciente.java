356ac6a89bff44bc81946e6259823ff997f114fb
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-02-11 22:47:55 -0300

    feat/30_implementa_tratamento_de_erros
    close #30
    Tratamento de erros: 404 e 400

--- api/src/main/java/med/voll/api/domain/Paciente/DadosCadastroPaciente.java
+++ api/src/main/java/med/voll/api/domain/Paciente/DadosCadastroPaciente.java
@@ -1,26 +1,30 @@
 package med.voll.api.domain.Paciente;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.domain.Endereco.Endereco;
 
 public record DadosCadastroPaciente(
 
-        @NotBlank
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
 
-        @NotBlank
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Formato do email é inválido")
         String email,
 
-        @NotBlank
+        @NotBlank(message = "Telefone é obrigatório")
         String telefone,
 
-        @NotBlank
+        @NotBlank(message = "CPF é obrigatório")
 //        @CPF
         String cpf,
 
-        @NotNull @Valid
+        @NotNull(message = "Dados do endereço são obrigatórios")
+        @Valid
         Endereco endereco
 ) {
 }
