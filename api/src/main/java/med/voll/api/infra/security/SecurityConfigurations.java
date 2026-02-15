1a8d33556f76cb863f20f5961283a4b3dd3e28b4
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-02-14 22:16:36 -0300

    Implementa autenticacao com login e senha

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,11 +1,18 @@
 package med.voll.api.infra.security;
 
+import med.voll.api.domain.Usuario.AutenticacaoService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -19,4 +26,13 @@ public class SecurityConfigurations {
                 .build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager  (AuthenticationConfiguration authConf) throws Exception {
+        return authConf.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
