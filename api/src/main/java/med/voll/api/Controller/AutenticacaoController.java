742e5523a25bce2a0b0b8fd1265e883703c1cc04
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-02-19 21:38:47 -0300

    Configura geracao de tokens

--- api/src/main/java/med/voll/api/Controller/AutenticacaoController.java
+++ api/src/main/java/med/voll/api/Controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package med.voll.api.Controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.Usuario.DadosAutenticacao;
+import med.voll.api.domain.Usuario.Usuario;
+import med.voll.api.domain.Usuario.UsuarioRepository;
+import med.voll.api.infra.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,12 +21,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
-    public ResponseEntity<Void> efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAutenticacao) {
+    public ResponseEntity<String> efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAutenticacao) {
 
         var token = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
---
+++ api/src/main/java/med/voll/api/infra/TokenService.java
@@ -0,0 +1,36 @@
+package med.voll.api.infra;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.Usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario){
+        try {
+            var algorithm = Algorithm.HMAC256("1234");
+            return JWT.create()
+                    .withIssuer("API VollMed")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException("Erro ao gerar Token JWT", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
