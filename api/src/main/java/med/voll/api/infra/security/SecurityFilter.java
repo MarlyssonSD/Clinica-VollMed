84483e60daf76fade5b22c68447ce0cd22bd1413
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-03-02 21:33:10 -0300

    Implementa controle de acesso e autenticacao
    close #37, #36

--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.Usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,9 +15,34 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+
+        if(tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = usuarioRepository.findByLogin(subject);
+
+            var autentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(autentication);
+        }
 
         filterChain.doFilter(request,response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var autorizationHeader = request.getHeader("Authorization");
+        if (autorizationHeader != null){
+            return  autorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
     }
 }
